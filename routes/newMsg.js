7e85b45ddb7471760d39dcc0e9a6510ca2f67ba2
tjjames <108950500+tjjames230@users.noreply.github.com>
2025-02-27 23:49:38 -0600

    updated post request

--- routes/newMsg.js
+++ routes/newMsg.js
@@ -5,8 +5,10 @@ import postMsg from "../controllers/postMsg.js";
 const newMsg = Router();
 
 newMsg.get("/", newMsgController);
-newMsg.post("/", (req, res, next) => {
-	console.log("test");
+newMsg.post("/", (req, res) => {
+	const msg = req.body.msg;
+	res.send(msg);
+	console.log(msg);
 });
 
 export default newMsg;
